4501cdad57d4994889eb8a21af3cfcb6357c22c3
JosteinLeander <jlelangmo@gmail.com>
2022-12-13 14:48:34 +0100

    Fikset på html

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//console.log("Hellow wolrd")
+document.getElementById("PSbutton").onclick = function () {
+  location.href = "/html/Playstation.html";
+};
+
+document.getElementById("Sbutton").onclick = function () {
+  location.href = "/html/Sega.html";
+};
+
+document.getElementById("Nbutton").onclick = function () {
+  location.href = "/html/Nintendo.html";
+};
+
+document.getElementById("Xbutton").onclick = function () {
+  location.href = "/html/Xbox.html";
+};
+
+document.getElementById("PS1button").onclick = function () {
+  location.href = "/html/Playstation.html";
+};
