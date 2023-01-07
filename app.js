6190921f17bc7c46591e4e82050278c12cd0b8df
JosteinLeander <jlelangmo@gmail.com>
2023-01-07 16:32:49 +0100

    Update all

--- app.js
+++ app.js
@@ -16,12 +16,21 @@ app.use(express.urlencoded({ extended: true }));
 //Firebase
 const firebase = require('firebase/app');
 const {
-    getFirestore, collection, onSnapshot,
-    addDoc, deleteDoc, doc,
-    query, where,
-    orderBy, serverTimeStamp,
-    getDoc, updateDoc, getDocs
+  getFirestore, collection, onSnapshot,
+  addDoc, deleteDoc, doc,
+  query, where,
+  orderBy, serverTimeStamp,
+  getDoc, updateDoc, getDocs
 } = require('firebase/firestore');
+
+// Firebase Auth
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} = require('firebase/auth');
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -39,6 +48,7 @@ firebase.initializeApp(firebaseConfig);
 
 //initialize services
 const db = getFirestore();
+const auth = getAuth();
 
 //collection ref
 const colRef = collection(db, 'produkter');
@@ -59,13 +69,14 @@ onSnapshot(q, (snapshot) => {
     console.log(toppListe);
 }) */
 
+// Query for å finne mest solgte mot db
 const q = query(colRef, orderBy('sold', 'desc'));
 
 let topSolgte = [];
 onSnapshot(q, (snapshot) => {
-    snapshot.docs.forEach(doc => {
-        topSolgte.push({ ...doc.data(), id: doc.id});
-    });
+  snapshot.docs.forEach(doc => {
+    topSolgte.push({ ...doc.data(), id: doc.id});
+  });
 });
 
 
@@ -78,8 +89,18 @@ app.get("/LoggInn", (req, res) => {
   res.render("LoggInn.ejs", { title: "LoggInn" });
 });
 
+app.post("/loggInnBruker", (req, res) => {
+  res.render("LoggInn.ejs", { title: "LoggInn" });
+});
+
 app.get("/NyBruker", (req, res) => {
-  res.render("NyBruker.ejs", { title: "NyBruker" });
+  console.log(req.body);
+  res.render("NyBruker.ejs", { title: "NyBruker"});
+});
+
+app.post("/regBruker", (req, res) => {
+  console.log(req.body);
+  res.render("NyBruker.ejs", { title: "NyBruker"});
 });
 
 app.get("/Products", (req, res) => {
@@ -88,70 +109,84 @@ app.get("/Products", (req, res) => {
 
 //Playstation
 app.get("/Playstation", (req, res) => {
-
-    const playstation = [];
-    topSolgte.forEach(doc => {
-      if (doc.type == "Playstation") {
-          playstation.push(doc);
-      }
-    });
-    
+  const playstation = [];
+  topSolgte.forEach(doc => {
+    if (doc.type == "Playstation") {
+      playstation.push(doc);
+    }
+  });
   res.render("Playstation.ejs", { title: "Playstation", playstation: playstation });
 });
 
 app.post("/Playstation", (req, res) => {
-    console.log(req.body)
-})
+  console.log(req.body)
+  res.send("playstation");
+});
 
 //Xbox
 app.get("/Xbox", (req, res) => {
-
-    const xbox = [];
-    topSolgte.forEach(doc => {
-        if (doc.type == "Xbox") {
-            xbox.push(doc);
-        }
-    });
-
+  const xbox = [];
+  topSolgte.forEach(doc => {
+    if (doc.type == "Xbox") {
+      xbox.push(doc);
+    }
+  });
   res.render("Xbox.ejs", { title: "Xbox", xbox: xbox });
 });
 
 app.post("/Xbox", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 });
 
 //Nintendo
 app.get("/Nintendo", (req, res) => {
-
   const nintendo = [];
   topSolgte.forEach(doc => {
     if (doc.type == "Nintendo") {
-        nintendo.push(doc);
+      nintendo.push(doc);
     }
   });
-  
-res.render("Nintendo.ejs", { title: "Nintendo", nintendo: nintendo });
+  res.render("Nintendo.ejs", { title: "Nintendo", nintendo: nintendo });
 });
 
 app.post("/Nintendo", (req, res) => {
   console.log(req.body)
-})
+});
 
 //Sega
 app.get("/Sega", (req, res) => {
-
   const sega = [];
   topSolgte.forEach(doc => {
     if (doc.type == "Sega") {
         sega.push(doc);
     }
   });
-  
-res.render("Sega.ejs", { title: "Sega", sega: sega });
+  res.render("Sega.ejs", { title: "Sega", sega: sega });
 });
 
 app.post("/Sega", (req, res) => {
   console.log(req.body)
-})
+});
 
- 
+app.get("/order", (req, res) => {
+  const order = topSolgte;
+  res.render("order.ejs", { title: "Handlekurv", order: order});
+});
+
+app.post("/credCard", (req, res) => {
+  const huh = req.body;
+  console.log("huh", huh);
+  res.redirect("/");
+});
+
+app.get("/search", (req, res) => {
+  const produkt = req.query.search;
+  const regex = new RegExp(produkt, "i");
+  const result = [];
+  topSolgte.forEach(doc => {
+    if (regex.test(JSON.stringify(doc))) {
+      result.push(doc);
+    }
+  });
+  res.render("Products.ejs", { title: "Produkter", prod: result});
+});
